d933f179e8250711e091cfd5070040116811b43b
SUNIL DHARAWAT <90829652+SunilDharawat@users.noreply.github.com>
2025-01-23 13:17:58 +0530

    Update HeaderIcons.jsx

--- src/components/header/HeaderIcons.jsx
+++ src/components/header/HeaderIcons.jsx
@@ -4,13 +4,13 @@ import {FaGithub} from 'react-icons/fa'
 function HeaderIcons() {
   return (
     <div className="header-icons">
-      <a href="https://www.linkedin.com/in/sunil-dharawat-a6726831a/" target="_blank">
+      <a href="https://www.linkedin.com/in/sunil-dharawat-a6726831a/" rel="noreferrer" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com/SunilDharawat" target="_blank">
+      <a href="https://github.com/SunilDharawat"  rel="noreferrer" target="_blank">
         <FaGithub />
       </a>
-      <a href="https://instagram.com" target="_blank">
+      <a href="https://instagram.com" rel="noreferrer"  target="_blank">
         <BsInstagram />
       </a>
     </div>
